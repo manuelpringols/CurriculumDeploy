9f7f70a1ed922ca08bbeb5b124cb7192528bbf5b
manuelpringols <manuelpringols@gmail.com>
2024-10-23 16:36:18 +0200

    deploy backend operativo rev:1.5.0

--- backend/src/main/java/com/example/curriculum/controller/ProjectController.java
+++ backend/src/main/java/com/example/curriculum/controller/ProjectController.java
@@ -17,7 +17,7 @@ public class ProjectController {
 	@Autowired
 	private ProjectService projectService;
 	
-	@GetMapping("/getName/{id}")
+	@GetMapping("/getName/{id}",produces = "application/json")
 	public ResponseEntity<?> getProjectName(@PathVariable Integer id){
 		
 		String projectName = this.projectService.getProjectName(id);
@@ -27,7 +27,7 @@ public class ProjectController {
 		
 	}
 	
-	@GetMapping("/getDescription/{id}")
+	@GetMapping("/getDescription/{id}",produces = "application/json")
 	public ResponseEntity<?> getProjectDescription(@PathVariable Integer id){
 		
 		String projectDescription = this.projectService.getProjectDescription(id);
