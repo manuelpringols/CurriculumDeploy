a355a97733c24d1d6640cb4741349c299f37cfae
manuelpringols <manuelpringols@gmail.com>
2024-10-23 16:06:47 +0200

    deploy backend operativo rev:1.1

--- backend/src/main/java/com/example/curriculum/config/WebConfig.java
+++ backend/src/main/java/com/example/curriculum/config/WebConfig.java
@@ -19,7 +19,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // consente tutte le richieste a tutti gli endpoint
-                .allowedOrigins("https://manuelpringols.github.io") // sostituisci con il tuo dominio GitHub Pages
+                .allowedOrigins("https://manuelpringols.github.io","http://188.245.185.96") // sostituisci con il tuo dominio GitHub Pages
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // specifica i metodi consentiti
                 .allowCredentials(true); // se hai bisogno di inviare credenziali
     }
