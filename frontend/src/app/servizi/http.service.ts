6c87b6212e225103b327e6548d106ea830bd34e1
manuelpringols <manuelpringols@gmail.com>
2024-10-23 18:02:51 +0200

    deploy backend operativo rev:1.6.9

--- frontend/src/app/servizi/http.service.ts
+++ frontend/src/app/servizi/http.service.ts
@@ -18,12 +18,12 @@ export class HttpService {
    }
 
    getProjectDescription(id:number):Observable<any>{
-   return this.http.get(`${this.apiUrl}/getDescription/${id} ` )
+   return this.http.get<string>(`${this.apiUrl}/getDescription/${id} `, )
    }
 
 
    getProjectTitle(id:number){
-    return this.http.get(`${this.apiUrl}/getTitle/${id}`)
+    return this.http.get<string>(`${this.apiUrl}/getTitle/${id}`)
     }
 
 
